0fce6d2d5bc250191b37a08c6a7f128da5d7fd35
Star_caorui <51264180+Star-caorui@users.noreply.github.com>
2019-06-04 21:43:26 +0800

    Update index.php

--- index.php
+++ index.php
@@ -14,7 +14,6 @@ else {
 session_start();
 $user_in = $_SESSION['user'];
 if (isset($_SESSION['user']) && !empty($_SESSION['user'])) {
-  echo "<script language=\"JavaScript\">alert(\"欢迎 {$user_in} 登入后台！\");</script>";
 } else {
   header('Location: login.php');
 }
@@ -22,69 +21,72 @@ if (isset($_SESSION['user']) && !empty($_SESSION['user'])) {
 
 //引入头部文件
 include "head.php";
+
+
+//声明变量
+$head = 'http://';
+$find_url = $head.$_SERVER['SERVER_NAME'].'/run.php?status=1&password='.$password;
+$fkz=file_get_contents($find_url);
+
+$str_meminfo = shell_exec('more /proc/meminfo');
+$pattern_meminfo = "/(.+):\s*([0-9]+)/";
+preg_match_all($pattern_meminfo, $str_meminfo, $out_meminfo);
+
+$str_cpu = shell_exec('more /proc/stat');
+$pattern_cpu = "/(cpu[0-9]?)[\s]+([0-9]+)[\s]+([0-9]+)[\s]+([0-9]+)[\s]+([0-9]+)[\s]+([0-9]+)[\s]+([0-9]+)[\s]+([0-9]+)/";
+preg_match_all($pattern_cpu, $str_cpu, $out_cpu);
+
+$n=0;
+
+
 ?>
+<meta http-equiv="refresh" content="3; url=<?php echo $head.$_SERVER['HTTP_HOST']; ?>">
   <h2 class="aui-bar">Minecraft 网页控制后台</h2> 
   <div class="aui-content aui-margin-b-15"> 
    <ul class="aui-list aui-list-in"> 
     <li class="aui-list-header">服务器状态</li> 
     <li class="aui-list-item"> 
-     <div class="aui-list-item-label-icon"> 
-      <i class="aui-iconfont aui-icon-<?php /* 预留，代码一会补全 */ ?>"></i> 
-     </div> 
      <div class="aui-list-item-inner"> 
-        <?php echo $user_in?>
-      <!--?php
-	$status = "mcchk -status ".$screen;
-        $cmd = system($status);
-        if ($cmd="0")
+
+<script type="text/javascript">
+var str = <?php echo "$fkz"; ?>;
+if (str!=0)
 {
-    echo "服务器未开启";
+	document.write("服务器已开启");
 }
 else
 {
-    echo "服务器已开启";
+	document.write("服务器未开启");
 }
-	?--> 
+</script>
+
+
      </div> </li> 
     <li class="aui-list-item">
-     <div class="aui-list-item-label-icon">
-      <i class="aui-iconfont aui-icon-edit"></i>
-     </div>
      <div class="aui-list-item-inner">
-       服务器IP/域名:<?php echo $ip;?>
+       IP/域名:<?php echo $ip;?>
      </div></li>
     <li class="aui-list-item"> 
-     <div class="aui-list-item-label-icon"> 
-      <i class="aui-iconfont aui-icon-camera"></i> 
-     </div> 
      <div class="aui-list-item-inner">
        IPv4端口:
       <?php echo $port; ?> 
      </div> </li> 
     <li class="aui-list-item"> 
-     <div class="aui-list-item-label-icon"> 
-      <i class="aui-iconfont aui-icon-camera"></i> 
-     </div> 
      <div class="aui-list-item-inner">
        CPU:
       <div class="aui-progress aui-progress-sm" style="margin:0 auto;width:75%"> 
-       <div class="aui-progress-bar" style="width: 60%;"></div> 
+       <div class="aui-progress-bar" style="width: <?php echo ceil((100*($out_cpu[1][$n]+$out_cpu[2][$n]+$out_cpu[3][$n])/($out_cpu[4][$n]+$out_cpu[5][$n]+$out_cpu[6][$n]+$out_cpu[7][$n])))."%"; ?>;"></div> 
       </div> 
      </div> </li> 
     <li class="aui-list-item"> 
-     <div class="aui-list-item-label-icon"> 
-      <i class="aui-iconfont aui-icon-camera"></i> 
-     </div> 
      <div class="aui-list-item-inner">
        内存:
       <div class="aui-progress aui-progress-sm" style="margin:0 auto;width:75%"> 
-       <div class="aui-progress-bar" style="width: 60%;"></div> 
+       <div class="aui-progress-bar" style="width: 
+<?php echo ceil((100*($out_meminfo[2][0]-$out_meminfo[2][1])/$out_meminfo[2][0]))."%"; ?>;"></div> 
       </div> 
      </div> </li> 
     <li class="aui-list-item"> 
-     <div class="aui-list-item-label-icon"> 
-      <i class="aui-iconfont aui-icon-camera"></i> 
-     </div> 
      <div class="aui-list-item-inner">
        硬盘:
       <div class="aui-progress aui-progress-sm" style="margin:0 auto;width:75%"> 
